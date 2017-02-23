a08453b8346d2e359d3df02242fda7b79d0c8887
styti0n <joaocarvalho9@gmail.com>
2017-02-23 20:20:08 +0000

    complete readDataSets

--- Source.cpp
+++ Source.cpp
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-void readDataSets(string nameFile) {
+vector<EndPoint> readDataSets(string nameFile) {
 	ifstream inFile(nameFile);
 	stringstream sstream;
 	string line;
@@ -23,13 +23,18 @@ void readDataSets(string nameFile) {
 	Video newVideo;
 	vector<Video> videos;
 
-	EndPoint newEndPoint;
 	vector<EndPoint> endPoints;
 
 	unsigned dataCenterLantency,
 		connectCachesNumber;
 
 	vector <Cache> caches;
+	unsigned cacheID, lantencyToEP;
+
+	unsigned videoID,
+		endPointID,
+		videoRequestNumbers;
+
 
 
 	for (int i = 0; i < 3; i++)
@@ -63,25 +68,69 @@ void readDataSets(string nameFile) {
 				sstream.clear();
 				line.clear();
 			}
-			if (endpointsNumber != 0)
+			for (int i = 0; i < endpointsNumber; i++)
 			{
-				for (int i = 0; i < endpointsNumber; i++)
+				EndPoint newEndPoint;
+				getline(inFile, line);
+				sstream << line;
+				sstream >> dataCenterLantency;
+				sstream >> connectCachesNumber;
+				newEndPoint.setLatencyToDataCenter(dataCenterLantency);
+				sstream.clear();
+				line.clear();
+				for (int j = 0; j < connectCachesNumber; j++)
 				{
 					getline(inFile, line);
 					sstream << line;
-					sstream >> dataCenterLantency;
-					sstream >> connectCachesNumber;
-					newEndPoint.setLatencyToDataCenter(dataCenterLantency);
+					sstream >> cacheID;
+					sstream >> lantencyToEP;
+					for (int k = 0; k < caches.size(); k++)
+					{
+						if (caches.at(k).getID() == cacheID)
+						{
+							CacheAndLatency newCacheAndLantecy;
+							newCacheAndLantecy.cache = &caches.at(k);
+							newCacheAndLantecy.latencyToEndp = lantencyToEP;
+							newEndPoint.addCaches(newCacheAndLantecy);
+							break;
+						}
+					}
 					sstream.clear();
 					line.clear();
-
 				}
-
+				endPoints.push_back(newEndPoint);
 			}
-
+			for (int i = 0; i < requestNumber; i++)
+			{
+				getline(inFile, line);
+				sstream << line;
+				sstream >> videoID;
+				sstream >> endPointID;
+				sstream >> videoRequestNumbers;
+				Request newRequest;
+				newRequest.numberOfRequests = videoRequestNumbers;
+				for (int j = 0; j < videos.size(); j++)
+				{
+					if (videos.at(j).ID == videoID)
+					{
+						newRequest.videoRequest = videos.at(j);
+						break;
+					}
+				}
+				for (int j = 0; j < endPoints.size(); j++)
+				{
+					if (endPoints.at(j).getID() == endPointID)
+					{
+						endPoints.at(j).addVideoRequest(newRequest);
+					}
+				}
+				sstream.clear();
+				line.clear();
+			}
+			inFile.close();
 		}
 	}
-
+	return endPoints;
 }
 
 struct Video
@@ -112,9 +161,15 @@ public:
 		this->ID = nextID;
 		nextID++;
 	}
+
+	unsigned getID() {
+		return ID;
+	}
+
 	void addVideo(Video video) {
 		videos.push_back(video);
 	}
+
 	void removeVideo(Video video) {
 		find(videos.begin(), videos.end(), video);
 	}
@@ -139,15 +194,20 @@ bool compareRequest(const Request& lhs, const Request&rhs)
 	return ((double)lhs.numberOfRequests / lhs.videoRequest.size < (double)rhs.numberOfRequests / rhs.videoRequest.size);
 }
 
+unsigned  EndPoint::nextID = 0;
 
 class EndPoint {
 private:
-
+	static unsigned nextID;
+	unsigned ID;
 	vector<CacheAndLatency> caches;
 	int dataCenterLatency;
 	vector<Request> videoRequests;
 public:
-	EndPoint();
+	EndPoint() {
+		this->ID = nextID;
+		nextID++;
+	}
 	void setCaches(vector<Cache> caches, vector<int> endPointToCachesLatency) {
 		CacheAndLatency a;
 		for (int i = 0; i < caches.size(); i++) {
@@ -157,14 +217,29 @@ public:
 		}
 		sort(caches.begin(), caches.end(), compareCaches);
 	}
+
+	void addCaches(CacheAndLatency a) {
+		caches.push_back(a);
+		sort(caches.begin(), caches.end(), compareCaches);
+	}
+
 	void setLatencyToDataCenter(int latency) {
 		dataCenterLatency = latency;
 	}
+
 	void setVideoRequests(vector<Request> requests) {
 		this->videoRequests = requests;
 		sort(videoRequests.begin(), videoRequests.end(), compareRequest);
 	}
 
+	void addVideoRequest(Request r) {
+		videoRequests.push_back(r);
+		sort(videoRequests.begin(), videoRequests.end(), compareRequest);
+	}
+
+	unsigned getID() {
+		return this->ID;
+	}
 
 	EndPoint(vector<Cache> caches, int dataCenterLatency, vector<int> endPointToCachesLatency, vector<Request> requests) {
 		CacheAndLatency a;
