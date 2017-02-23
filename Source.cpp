704d351548a41bd4cf3a0047fad568f3f4d56e5d
ineeve <renatoineeve@gmail.com>
2017-02-23 20:31:15 +0000

    Last

--- Source.cpp
+++ Source.cpp
@@ -9,6 +9,174 @@
 
 using namespace std;
 
+
+
+struct Video
+{
+	unsigned ID;
+	unsigned size;
+};
+
+struct Request
+{
+	Video videoRequest;
+	unsigned numberOfRequests;
+};
+
+
+
+class Cache
+{
+private:
+	static unsigned nextID;
+	static unsigned maxSize;
+	unsigned ID;
+	unsigned actualSize;
+	vector<Video> videos;
+
+public:
+	Cache(unsigned maxSize) {
+		this->ID = nextID;
+		nextID++;
+	}
+	int getID() {
+		return ID;
+	}
+	vector<Video> getVideos() {
+		return videos;
+	}
+	void addVideo(Video video) {
+		videos.push_back(video);
+		actualSize -= video.size;
+	}
+
+	void removeVideo(Video video) {
+		find(videos.begin(), videos.end(), video);
+	}
+	unsigned int getActualSize() {
+		return actualSize;
+	}
+	unsigned int getMaxSize() {
+		return maxSize;
+	}
+};
+unsigned Cache::nextID = 0;
+
+struct CacheAndLatency {
+	Cache *cache;
+	int latencyToEndp;
+};
+
+bool compareCaches(const CacheAndLatency& lhs, const CacheAndLatency&rhs)
+{
+	if (lhs.latencyToEndp < rhs.latencyToEndp) {
+		return true;
+	}
+	return false;
+}
+
+bool compareRequest(const Request& lhs, const Request&rhs)
+{
+	return ((double)lhs.numberOfRequests / lhs.videoRequest.size < (double)rhs.numberOfRequests / rhs.videoRequest.size);
+}
+
+
+
+class EndPoint {
+private:
+	static unsigned nextID;
+	unsigned ID;
+	vector<CacheAndLatency> caches;
+	int dataCenterLatency;
+	vector<Request> videoRequests;
+public:
+	EndPoint() {
+		this->ID = nextID;
+		nextID++;
+	}
+	vector<CacheAndLatency> getCaches() {
+		return caches;
+	}
+
+	void setCaches(vector<Cache> caches, vector<int> endPointToCachesLatency) {
+		CacheAndLatency a;
+		for (int i = 0; i < caches.size(); i++) {
+			a.cache = &caches.at(i);
+			a.latencyToEndp = endPointToCachesLatency.at(i);
+			this->caches.push_back(a);
+		}
+		sort(caches.begin(), caches.end(), compareCaches);
+	}
+
+	void addCaches(CacheAndLatency a) {
+		caches.push_back(a);
+		sort(caches.begin(), caches.end(), compareCaches);
+	}
+
+	void setLatencyToDataCenter(int latency) {
+		dataCenterLatency = latency;
+	}
+
+	void setVideoRequests(vector<Request> requests) {
+		this->videoRequests = requests;
+		sort(videoRequests.begin(), videoRequests.end(), compareRequest);
+	}
+
+	void addVideoRequest(Request r) {
+		videoRequests.push_back(r);
+		sort(videoRequests.begin(), videoRequests.end(), compareRequest);
+	}
+
+	unsigned getID() {
+		return this->ID;
+	}
+
+	EndPoint(vector<Cache> caches, int dataCenterLatency, vector<int> endPointToCachesLatency, vector<Request> requests) {
+		CacheAndLatency a;
+		for (int i = 0; i < caches.size(); i++) {
+			a.cache = &caches.at(i);
+			a.latencyToEndp = endPointToCachesLatency.at(i);
+			this->caches.push_back(a);
+		}
+		sort(caches.begin(), caches.end(), compareCaches);
+		this->dataCenterLatency = dataCenterLatency;
+		this->videoRequests = requests;
+		sort(videoRequests.begin(), videoRequests.end(), compareRequest);
+	}
+	void addVideosToCaches() {
+		for (int k = 0; k < caches.size(); k++) {
+			while (videoRequests.size() >= 0 && caches.at(k).cache->getActualSize() > videoRequests.at(0).videoRequest.size) {
+				caches.at(k).cache->addVideo(videoRequests.at(0).videoRequest);
+				videoRequests.erase(videoRequests.begin());
+			}
+			
+		}
+		
+	}
+
+};
+unsigned  EndPoint::nextID = 0;
+
+void printResult(vector<EndPoint> &endpoints) {
+	int cachesUsed = 0;
+	for (int i = 0; i < endpoints.size(); i++) {
+		for (int c = 0; c < endpoints.at(i).getCaches().size(); c++) {
+			if (endpoints.at(i).getCaches().at(c).cache->getActualSize() != endpoints.at(i).getCaches().at(c).cache->getMaxSize()) {
+				cachesUsed++;
+			}
+		}
+	}
+	for (int i = 0; i < endpoints.size(); i++) {
+		for (int c = 0; c < endpoints.at(i).getCaches().size(); c++) {
+			cout << endpoints.at(i).getCaches().at(c).cache->getID();
+			for (int v = 0; v < endpoints.at(i).getCaches().at(c).cache->getVideos().size(); v++) {
+				cout << " " << endpoints.at(i).getCaches().at(c).cache->getVideos().at(v).ID;
+			}
+			cout << "\n";
+ 		}
+	}
+}
+
 vector<EndPoint> readDataSets(string nameFile) {
 	ifstream inFile(nameFile);
 	stringstream sstream;
@@ -132,172 +300,6 @@ vector<EndPoint> readDataSets(string nameFile) {
 	}
 	return endPoints;
 }
-
-struct Video
-{
-	unsigned ID;
-	unsigned size;
-};
-
-struct Request
-{
-	Video videoRequest;
-	unsigned numberOfRequests;
-};
-
-unsigned Cache::nextID = 0;
-
-class Cache
-{
-private:
-	static unsigned nextID;
-	static unsigned maxSize;
-	unsigned ID;
-	unsigned actualSize;
-	vector<Video> videos;
-
-public:
-	Cache(unsigned maxSize) {
-		this->ID = nextID;
-		nextID++;
-	}
-	int getID() {
-		return ID;
-	}
-	vector<Video> getVideos();
-	void addVideo(Video video) {
-		videos.push_back(video);
-		actualSize -= video.size;
-	}
-
-	void removeVideo(Video video) {
-		find(videos.begin(), videos.end(), video);
-	}
-	unsigned int getActualSize() {
-		return actualSize;
-	}
-	unsigned int getMaxSize() {
-		return maxSize;
-	}
-};
-
-
-struct CacheAndLatency {
-	Cache *cache;
-	int latencyToEndp;
-};
-
-bool compareCaches(const CacheAndLatency& lhs, const CacheAndLatency&rhs)
-{
-	if (lhs.latencyToEndp < rhs.latencyToEndp) {
-		return true;
-	}
-	return false;
-}
-
-bool compareRequest(const Request& lhs, const Request&rhs)
-{
-	return ((double)lhs.numberOfRequests / lhs.videoRequest.size < (double)rhs.numberOfRequests / rhs.videoRequest.size);
-}
-
-unsigned  EndPoint::nextID = 0;
-
-class EndPoint {
-private:
-	static unsigned nextID;
-	unsigned ID;
-	vector<CacheAndLatency> caches;
-	int dataCenterLatency;
-	vector<Request> videoRequests;
-public:
-	EndPoint() {
-		this->ID = nextID;
-		nextID++;
-	}
-	vector<CacheAndLatency> getCaches() {
-		return caches;
-	}
-
-	void setCaches(vector<Cache> caches, vector<int> endPointToCachesLatency) {
-		CacheAndLatency a;
-		for (int i = 0; i < caches.size(); i++) {
-			a.cache = &caches.at(i);
-			a.latencyToEndp = endPointToCachesLatency.at(i);
-			this->caches.push_back(a);
-		}
-		sort(caches.begin(), caches.end(), compareCaches);
-	}
-
-	void addCaches(CacheAndLatency a) {
-		caches.push_back(a);
-		sort(caches.begin(), caches.end(), compareCaches);
-	}
-
-	void setLatencyToDataCenter(int latency) {
-		dataCenterLatency = latency;
-	}
-
-	void setVideoRequests(vector<Request> requests) {
-		this->videoRequests = requests;
-		sort(videoRequests.begin(), videoRequests.end(), compareRequest);
-	}
-
-	void addVideoRequest(Request r) {
-		videoRequests.push_back(r);
-		sort(videoRequests.begin(), videoRequests.end(), compareRequest);
-	}
-
-	unsigned getID() {
-		return this->ID;
-	}
-
-	EndPoint(vector<Cache> caches, int dataCenterLatency, vector<int> endPointToCachesLatency, vector<Request> requests) {
-		CacheAndLatency a;
-		for (int i = 0; i < caches.size(); i++) {
-			a.cache = &caches.at(i);
-			a.latencyToEndp = endPointToCachesLatency.at(i);
-			this->caches.push_back(a);
-		}
-		sort(caches.begin(), caches.end(), compareCaches);
-		this->dataCenterLatency = dataCenterLatency;
-		this->videoRequests = requests;
-		sort(videoRequests.begin(), videoRequests.end(), compareRequest);
-	}
-	void addVideosToCaches() {
-		for (int k = 0; k < caches.size(); k++) {
-			while (videoRequests.size() >= 0 && caches.at(k).cache->getActualSize() > videoRequests.at(0).videoRequest.size) {
-				caches.at(k).cache->addVideo(videoRequests.at(0).videoRequest);
-				videoRequests.erase(videoRequests.begin());
-			}
-			
-		}
-		
-	}
-
-};
-
-
-void printResult(vector<EndPoint> &endpoints) {
-	int cachesUsed = 0;
-	for (int i = 0; i < endpoints.size(); i++) {
-		for (int c = 0; c < endpoints.at(i).getCaches().size(); c++) {
-			if (endpoints.at(i).getCaches().at(c).cache->getActualSize() != endpoints.at(i).getCaches().at(c).cache->getMaxSize()) {
-				cachesUsed++;
-			}
-		}
-	}
-	for (int i = 0; i < endpoints.size(); i++) {
-		for (int c = 0; c < endpoints.at(i).getCaches().size(); c++) {
-			cout << endpoints.at(i).getCaches().at(c).cache->getID();
-			for (int v = 0; v < endpoints.at(i).getCaches().at(c).cache->getVideos().size(); v++) {
-				cout << " " << endpoints.at(i).getCaches().at(c).cache->getVideos().at(v).ID;
-			}
-			cout << "\n";
- 		}
-	}
-}
-
-
 int main() {
 	string filename = "kittens.in";
 	vector<EndPoint> endPoints = readDataSets(filename);
