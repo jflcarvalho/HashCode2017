c48e588a9d13416b7e70ad98aaa2a0d0a4beef57
styti0n <joaocarvalho9@gmail.com>
2017-02-23 18:08:33 +0000

    add source file

---
+++ Source.cpp
@@ -0,0 +1,25 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void readDataSets(string nameFile){
+	ifstream inFile(nameFile);
+	string line;
+
+	for (int i = 0; i < 3; i++)
+	{
+		if (inFile.is_open())
+		{
+			while (getline(inFile, line)){
+			{
+
+			}
+
+}
+
+int main() {
+
+	return 0;
+}
